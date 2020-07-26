5a5daf44fb537bdc78853f300eb1a3010f3ac5aa
vvsolo <43495170+vvsolo@users.noreply.github.com>
2020-07-27 00:03:52 +0800

    PWA

--- sw.js
+++ sw.js
@@ -11,30 +11,3 @@ self.addEventListener('install',e =>{
     .then(() => self.skipWaiting())
   )
 })
-self.addEventListener('fetch',function(e){
-  e.respondWith(
-    caches.match(e.request).then(function(response){
-      if(response != null){
-        return response
-      }
-      return fetch(e.request.url)
-    })
-  )
-})
-self.addEventListener('activate',function(e){
-  e.waitUntil(
-    //获取所有cache名称
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        // 获取所有不同于当前版本名称cache下的内容
-        cacheNames.filter(cacheNames => {
-          return cacheNames !== cacheStorageKey
-        }).map(cacheNames => {
-          return caches.delete(cacheNames)
-        })
-      )
-    }).then(() => {
-      return self.clients.claim()
-    })
-  )
-})
