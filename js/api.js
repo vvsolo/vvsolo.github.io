54f624244f3060c53276a286f7120eaf2e5fa859
vsolo <43495170+vvsolo@users.noreply.github.com>
2022-05-06 00:10:34 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -134,7 +134,7 @@ function onTypeSetSplit(str, author, site) {
 		'y': '是',
 		'd': new Intl.DateTimeFormat('zh-CN', {year: 'numeric', month: '2-digit', day: '2-digit'}).format(new Date()),
 		'n': (str.length - str.findCount(/[　\s]/g)).toLocaleString()
-	}) + str + '\n\n';
+	}) + '\n\n' + str + '\n\n';
 }
 
 // 一键整理
