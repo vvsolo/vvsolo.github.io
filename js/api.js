d4b4b658362b77318959c0e8e2f08f7812e2cb13
vsolo <43495170+vvsolo@users.noreply.github.com>
2022-05-08 16:15:42 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -134,7 +134,7 @@ function onTypeSetSplit(str, author, site, calcspace) {
 		'b': site,
 		'y': '是',
 		'd': new Intl.DateTimeFormat('zh-CN', {year: 'numeric', month: '2-digit', day: '2-digit'}).format(new Date()),
-		'n': (str.length - (calcspace == '1' ? str.findCount(/[\s]/g) : str.findCount(/[　\s]/g))).toLocaleString()
+		'n': (str.length - (String(calcspace) == '1' ? str.findCount(/[\s]/g) : str.findCount(/[　\s]/g))).toLocaleString()
 	}) + '\n\n' + str + '\n\n';
 }
 
